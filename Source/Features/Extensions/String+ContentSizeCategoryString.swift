c2b6c0a06ee70933fa3912c891cb6ccbb62ff2a5
Christoph Wendt <christoph.wendt@me.com>
2018-07-06 23:10:12 +0200

    chore: add extension to map watch text size to string

--- Source/Features/Extensions/String+ContentSizeCategoryString.swift
+++ Source/Features/Extensions/String+ContentSizeCategoryString.swift
@@ -1,8 +1,31 @@
 //
-//  String+ContentCategory.swift
+//  CGFloat+ContentSizeCategoryString.swift
 //  Capable
 //
 //  Created by Christoph Wendt on 06.07.18.
 //
 
-import Foundation
+#if os(watchOS) || os(tvOS)
+extension CGFloat {
+    var contentSizeString: String {
+        get {
+            switch(self){
+            case 14.0:
+                return "XS"
+            case 15.0:
+                return "S"
+            case 16.0:
+                return "L"
+            case 17.0:
+                return "XL"
+            case 18.0:
+                return "XXL"
+            case 19.0:
+                return "XXXL"
+            default:
+                return "Unknown"
+            }
+        }
+    }
+}
+#endif
