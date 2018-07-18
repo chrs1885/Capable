81b3881d726d652bc883a7dc3eeae2403a1e5790
Christoph Wendt <christoph.wendt@me.com>
2018-07-18 19:48:17 +0200

    chore: add setup script for installing dependencies

---
+++ Contribute/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Install Jazzy for generating code documentation (https://github.com/realm/jazzy)
+gem install jazzy
+
+# Install SwiftLint to enforce the Capable coding style & conventions
+brew install swiftlint
