23a9039fa9dd56f76927525387b86654fa42805f
Christoph Wendt <christoph.wendt@me.com>
2018-08-15 17:34:34 +0200

    chore: add implementations for Handicap and HandicapEnabledMode

---
+++ Source/Features/Models/Handicap.swift
@@ -0,0 +1,34 @@
+//
+//  Handicap.swift
+//  Capable
+//
+//  Created by Christoph Wendt on 15.08.18.
+//
+
+/**
+ Model class that groups a number of `CapableFeature`s to represent a user's handicap. */
+public struct Handicap {
+
+    /// A list of `CapableFeature`s that are expected to be enabled if a user has this handicap.
+    public private(set) var features: [CapableFeature]
+
+    /// The name of the `Handicap` that can be used to uniquely identify the `Handicap`. This name is also used inside the status map.
+    public private(set) var name: String
+
+    /// This mode defines whether all features need to be enabled to set the `Handicap`'s status to enabled or only one of them.
+    public private(set) var enabledIf: HandicapEnabledMode
+
+    /**
+     Initializes a new `Handicap` object.
+
+     - Parameters:
+     - features: A list of features that are expected to be enabled if a user has this handicap.
+     - name: The name of the `Handicap` that can be used to uniquely identify the `Handicap`. This name is also used inside the status map.
+     - enabledIf: This mode defines whether all features need to be enabled to set the `Handicap`'s status to enabled or only one of them.
+     */
+    init(with features: [CapableFeature], name: String, enabledIf: HandicapEnabledMode) {
+        self.features = features
+        self.name = name
+        self.enabledIf = enabledIf
+    }
+}
