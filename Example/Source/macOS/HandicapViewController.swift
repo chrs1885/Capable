f51775d97a64ed4fc235c8d6b3e0e88fb19b2f6b
Christoph Wendt <christoph.wendt@me.com>
2018-08-19 22:12:53 +0200

    chore: add macOS example

---
+++ Example/Source/macOS/HandicapViewController.swift
@@ -0,0 +1,75 @@
+//
+//  HandicapViewController.swift
+//  Example-macOS
+//
+//  Created by Christoph Wendt on 19.08.18.
+//  Copyright © 2018 Christoph Wendt. All rights reserved.
+//
+
+import Cocoa
+import Capable
+
+class HandicapViewController: NSViewController {
+    @IBOutlet weak var handicapTableView: NSTableView!
+    var capable: Capable?
+    var objects: [String: String]?
+    var handicaps: [Handicap]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let blindness = Handicap(with: [.invertColors, .voiceOver], name: "Blindness", enabledIf: .oneFeatureEnabled)
+        self.capable = Capable(withHandicaps: [blindness])
+        self.handicaps = [blindness]
+        refreshData()
+    }
+
+    func refreshData() {
+        if let capable = self.capable {
+            self.objects = capable.statusMap
+        }
+    }
+}
+
+// MARK: - Table View
+extension HandicapViewController: NSTableViewDataSource, NSTableViewDelegate{
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return objects?.count ?? 0
+    }
+
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView?{
+        let cell: HandicapTableCellView = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "HandicapRow"), owner: self) as! HandicapTableCellView
+        let status = self.value(forRow: row)
+        let handicap = self.handicap(forName: status.key)
+        cell.textLabel!.stringValue = handicap.name
+        cell.subtitleTextLabel!.stringValue = CapableFeature.keys(forFeatures: handicap.features).joined(separator: ", ")
+        cell.detailTextLabel!.stringValue = status.value
+
+        return cell
+    }
+
+    private func value(forRow row: Int) -> (key: String, value: String) {
+        if let objects = self.objects {
+            let featuresArray = Array(objects)
+            return featuresArray[row]
+        }
+        fatalError("Requested item does not exist")
+    }
+
+    private func handicap(forName name: String) -> Handicap {
+        for handicap in self.handicaps! {
+            if handicap.name == name {
+                return handicap
+            }
+        }
+        fatalError("Requested item does not exist")
+    }
+}
+
+// MARK: - Toolbar actions
+extension HandicapViewController {
+    @IBAction func refresh(_ sender: Any) {
+        self.refreshData()
+        self.handicapTableView.reloadData()
+    }
+}
+
