0b5145a34d61cd1f8947a78427b38c4d23e72ce2
Christoph Wendt <christoph.wendt@me.com>
2018-10-14 23:08:49 +0200

    chore: update tvOS example app to use swift 4.2

--- Example/Source/AppDelegate.swift
+++ Example/Source/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
