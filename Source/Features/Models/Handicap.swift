966e491c0673dea7f1ef7b448bfa61f4fdf5ff97
Christoph Wendt <christoph.wendt@me.com>
2018-10-29 22:41:53 +0100

    refactor: remove default init function from struct since it gets provided automatically

--- Source/Features/Models/Handicap.swift
+++ Source/Features/Models/Handicap.swift
@@ -8,7 +8,7 @@
 /**
  Model class that groups a number of `CapableFeature`s to represent a user's handicap.
  */
-public struct Handicap {
+public struct Handicap: Equatable {
 
     /// A list of `CapableFeature`s that are expected to be enabled if a user has this handicap.
     public private(set) var features: [CapableFeature]
@@ -18,29 +18,4 @@ public struct Handicap {
 
     /// This mode defines whether all features need to be enabled to set the `Handicap`'s status to enabled or only one of them.
     public private(set) var enabledIf: HandicapEnabledMode
-
-    /**
-     Initializes a new `Handicap` object.
-
-     - Parameters:
-     - features: A list of features that are expected to be enabled if a user has this handicap.
-     - name: The name of the `Handicap` that can be used to uniquely identify the `Handicap`. This name is also used inside the status map.
-     - enabledIf: This mode defines whether all features need to be enabled to set the `Handicap`'s status to enabled or only one of them.
-     */
-    public init(with features: [CapableFeature], name: String, enabledIf: HandicapEnabledMode) {
-        self.features = features
-        self.name = name
-        self.enabledIf = enabledIf
-    }
-}
-
-// MARK: - Equatable
-/// Compares to Handicap instances with one of each other.
-extension Handicap: Equatable {
-    public static func == (lhs: Handicap, rhs: Handicap) -> Bool {
-        return
-            lhs.features == rhs.features &&
-            lhs.name == rhs.name &&
-            lhs.enabledIf == rhs.enabledIf
-    }
 }
