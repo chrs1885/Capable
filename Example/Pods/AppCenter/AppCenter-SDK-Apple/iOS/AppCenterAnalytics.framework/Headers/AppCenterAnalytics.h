bf833ba2205a546af6f7a514fb59e7579989af01
Christoph Wendt <christoph.wendt@me.com>
2018-11-12 21:33:16 +0100

    chore: remove pods from source control

--- Example/Pods/AppCenter/AppCenter-SDK-Apple/iOS/AppCenterAnalytics.framework/Headers/AppCenterAnalytics.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-#import "MSAbstractLog.h"
-#import "MSAnalytics.h"
-#import "MSAnalyticsTransmissionTarget.h"
-#import "MSAnalyticsAuthenticationProvider.h"
-#import "MSAnalyticsAuthenticationProviderDelegate.h"
-#import "MSEventLog.h"
