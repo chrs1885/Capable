0c9bfab342a5d053f5a8b6bb1dfa535357ff81a1
Christoph Wendt <christoph.wendt@me.com>
2019-01-16 22:21:05 +0100

    feature: expose functionality via NSColor extension

--- Source/Colors/Extensions/NSColor+wcag.swift
+++ Source/Colors/Extensions/NSColor+wcag.swift
@@ -9,30 +9,121 @@
 
 import AppKit
 
-/// Extension that adds functionality for calculating WCAG colors.
+/// Extension that adds functionality for calculating colors that conform to WCAG success criterias.
 extension NSColor {
 
     /**
-     Calculates the color ratio of two given colors.
+     Calculates the color ratio for a text color on a background color.
 
      - Parameters:
-     - aColor: The first color.
-     - otherColor: The second color.
+         - textColor: The text color.
+         - backgroundColor: The background color.
 
      - Returns: The contrast ratio for a given pair of colors.
+
+     - Warning: This function only accepts RGB compatible colors and will fail otherwise.
      */
-    public class func getContrastRatio(forColor aColor: NSColor, andColor otherColor: NSColor) -> CGFloat {
-        guard let color1 = aColor.rgbaColor, let color2 = otherColor.rgbaColor else {
-            return 0.0
-        }
-        return RGBAColor.getContrastRatio(forColor: color1, andColor: color2)
+    public class func getContrastRatio(forTextColor textColor: NSColor, onBackgroundColor backgroundColor: NSColor) -> CGFloat {
+        let textColor = textColor.rgbaColor
+        let backgroundColor = backgroundColor.rgbaColor
+
+        return RGBAColor.getContrastRatio(forTextColor: textColor, onBackgroundColor: backgroundColor)
     }
 
-    var rgbaColor: RGBAColor? {
-        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
-        self.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+    /**
+     Returns the text color with the highest contrast (black or white) for a given background color.
 
-        return RGBAColor(red: red, green: green, blue: blue, alpha: alpha)
+     - Parameters:
+        - backgroundColor: The background color.
+
+     - Returns: A color that has the highest contrast with the given background color.
+
+     - Warning: This function only accepts RGB compatible colors and will fail otherwise.
+     */
+    public class func getTextColor(onBackgroundColor backgroundColor: NSColor) -> NSColor {
+        let textColor = RGBAColor.getTextColor(onBackgroundColor: backgroundColor.rgbaColor)
+
+        return textColor == RGBAColor.Colors.black ? .black : .white
+    }
+
+    /**
+     Calculates the contrast ratio of a given list of text colors and a background color. The first color that conforms to the conformance level defined gets returned. The default conformance level is .AA.
+
+     - Parameters:
+         - colors: A list of possible text colors.
+         - font: The font used for the text.
+         - backgroundColor: The background color that the text should be displayed on.
+         - conformanceLevel: The conformance level that needs to be passed when calculating the contrast ratio. The default conformance level is .AA.
+
+     - Returns: The first color that conforms to the conformance level defined.
+
+     - Warning: This function only accepts RGB compatible colors and will fail otherwise.
+     */
+    public class func getTextColor(fromColors colors: [NSColor] = [], withFont font: NSFont, onBackgroundColor backgroundColor: NSColor, conformanceLevel: ConformanceLevel = .AA) -> NSColor? {
+        let backgroundColor = backgroundColor.rgbaColor
+        for textColor in colors {
+            let isValidTextColor = RGBAColor.isValidColorCombination(textColor: textColor.rgbaColor, fontProps: font.fontProps, onBackgroundColor: backgroundColor, conformanceLevel: conformanceLevel)
+            if isValidTextColor {
+                return textColor
+            }
+        }
+
+        return nil
+    }
+
+    /**
+     Returns the background color with the highest contrast (black or white) for a given text color.
+
+     - Parameters:
+        - textColor: The textColor color.
+
+     - Returns: A color that has the highest contrast with the given text color.
+
+     - Warning: This function only accepts RGB compatible colors and will fail otherwise.
+     */
+    public class func getBackgroundColor(forTextColor textColor: NSColor) -> NSColor {
+        let backgroundColor = RGBAColor.getBackgroundColor(forTextColor: textColor.rgbaColor)
+
+        return backgroundColor == RGBAColor.Colors.black ? .black : .white
+    }
+
+    /**
+     Calculates the contrast ratio of a given list of background colors and a text color. The first color that conforms to the conformance level defined gets returned. The default conformance level is .AA.
+
+     - Parameters:
+         - colors: A list of possible background colors.
+         - font: The font used for the text.
+         - textColor: The text color that should be used.
+         - conformanceLevel: The conformance level that needs to be passed when calculating the contrast ratio. The default conformance level is .AA.
+
+     - Returns: The first color that conforms to the conformance level defined.
+
+     - Warning: This function only accepts RGB compatible colors and will fail otherwise.
+     */
+    public class func getBackgroundColor(fromColors colors: [NSColor], withFont font: NSFont, belowTextColor textColor: NSColor, conformanceLevel: ConformanceLevel = .AA) -> NSColor? {
+        let textColor = textColor.rgbaColor
+
+        for backgroundColor in colors {
+            let isValidBackgroundColor = RGBAColor.isValidColorCombination(textColor: textColor, fontProps: font.fontProps, onBackgroundColor: backgroundColor.rgbaColor, conformanceLevel: conformanceLevel)
+            if isValidBackgroundColor {
+                return backgroundColor
+            }
+        }
+
+        return nil
+    }
+
+    var rgbaColor: RGBAColor {
+        guard let rgbColor = self.usingColorSpace(.sRGB) else {
+            let errorMessage = "Calculating WCAG compliant colors is only supported for RGB compatible input colors."
+            Logger.error(errorMessage)
+            fatalError(errorMessage)
+        }
+
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        rgbColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+        return RGBAColor(red: red*255, green: green*255, blue: blue*255, alpha: alpha)
     }
 }
 
---
+++ Tests/Colors/NSColorWcagTests.swift
@@ -0,0 +1,309 @@
+//
+//  NSColorWcagTests.swift
+//  CapableTests
+//
+//  Created by Christoph Wendt on 14.01.19.
+//
+
+#if os(OSX)
+
+import Quick
+import Nimble
+@testable import Capable
+
+class NSColorWcagTests: QuickSpec {
+    override func spec() {
+        struct Colors {
+            static let white = NSColor(deviceRed: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            static let black = NSColor(deviceRed: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+            static let colorWithContrastRatio3 = NSColor(deviceRed: 148/255.0, green: 148/255.0, blue: 148/255.0, alpha: 1.0)
+            static let colorWithContrastRatio4_5 = NSColor(deviceRed: 118/255.0, green: 118/255.0, blue: 118/255.0, alpha: 1.0)
+            static let colorWithContrastRatio7 = NSColor(deviceRed: 89/255.0, green: 89/255.0, blue: 89/255.0, alpha: 1.0)
+            static let semiTransparentColor = NSColor(deviceRed: 0.5, green: 0.0, blue: 1.0, alpha: 0.75)
+            static let grayscaleColor = NSColor.white
+            static let cmykColor = NSColor(deviceCyan: 1.0, magenta: 0.0, yellow: 0.0, black: 0.0, alpha: 1.0)
+            static let p3Color = NSColor(displayP3Red: 2.0, green: -1.0, blue: 0.5, alpha: 1.0)
+        }
+
+        struct Fonts {
+            static let smallFont = NSFont.systemFont(ofSize: 14.0)
+            static let smallBoldFont = NSFont.boldSystemFont(ofSize: 14.0)
+            static let largeFont = NSFont.systemFont(ofSize: 18.0)
+        }
+
+        describe("The NSColor class") {
+            context("when calling rgbaColor") {
+                context("when color space is sRGB") {
+                    it("normalizes components to be between 0.0 and 255.0") {
+                        let color = Colors.white.rgbaColor
+
+                        expect(color.red).to(equal(255.0))
+                        expect(color.green).to(equal(255.0))
+                        expect(color.blue).to(equal(255.0))
+                        expect(color.alpha).to(equal(1.0))
+                    }
+                }
+
+                context("when color space is P3") {
+                    it("normalizes components to sRGB") {
+                        let color = Colors.p3Color.rgbaColor
+
+                        expect(color.red).to(equal(255.0))
+                        expect(color.green).to(equal(0.0))
+                        expect(floor(color.blue)).to(equal(127.0))
+                        expect(color.alpha).to(equal(1.0))
+                    }
+                }
+
+                context("when color space is Grayscale") {
+                    it("uses white value for each color component") {
+                        let color = Colors.grayscaleColor.rgbaColor
+                        let whiteComponent = Colors.grayscaleColor.whiteComponent*255.0
+                        let alphaComponent = Colors.grayscaleColor.alphaComponent
+
+                        expect(color.red).to(equal(whiteComponent))
+                        expect(color.green).to(equal(whiteComponent))
+                        expect(color.blue).to(equal(whiteComponent))
+                        expect(color.alpha).to(equal(alphaComponent))
+                    }
+                }
+
+                context("when color space is CMYK compatible") {
+                    it("converts the color to sRGB") {
+                        expect(Colors.cmykColor.rgbaColor).toNot(throwError())
+                    }
+                }
+            }
+
+            context("when calling getContrastRatio") {
+                context("by passing in the same color twice") {
+                    it("returns the minimum contrast ratio of 1.0") {
+                        expect(NSColor.getContrastRatio(forTextColor: Colors.white, onBackgroundColor: Colors.white)).to(equal(1.0))
+                    }
+                }
+
+                context("by passing in white and black") {
+                    it("returns the maximum contrast ratio of 21.0") {
+                        expect(NSColor.getContrastRatio(forTextColor: Colors.white, onBackgroundColor: Colors.black)).to(equal(21.0))
+                    }
+                }
+
+                context("by passing in green and orange color") {
+                    it("returns a contrast ratio of 2.31") {
+                        let actualContrastRatio = NSColor.getContrastRatio(forTextColor: Colors.colorWithContrastRatio3, onBackgroundColor: Colors.colorWithContrastRatio7)
+                        let rounded = floor(actualContrastRatio * 100) / 100
+
+                        expect(rounded).to(equal(2.3))
+                    }
+                }
+
+                context("by passing in semi transparent color and white") {
+                    it("returns a contrast ratio of 4.51") {
+                        let actualContrastRatio = NSColor.getContrastRatio(forTextColor: Colors.semiTransparentColor, onBackgroundColor: Colors.white)
+                        let rounded = floor(actualContrastRatio * 100) / 100
+
+                        expect(rounded).to(equal(4.51))
+                    }
+                }
+            }
+
+            context("when calling getTextColor") {
+                context("by passing in a dark background color") {
+                    it("returns white") {
+                        let actual = NSColor.getTextColor(onBackgroundColor: Colors.black)
+
+                        expect(actual).to(equal(NSColor.white))
+                    }
+                }
+
+                context("by passing in a light background color") {
+                    it("returns black") {
+                        let actual = NSColor.getTextColor(onBackgroundColor: Colors.white)
+
+                        expect(actual).to(equal(NSColor.black))
+                    }
+                }
+
+                context("by passing in a background color with a medium relative luminance") {
+                    it("returns black") {
+                        let actual = NSColor.getTextColor(onBackgroundColor: Colors.colorWithContrastRatio4_5)
+
+                        expect(actual).to(equal(NSColor.black))
+                    }
+                }
+            }
+
+            context("when calling getTextColor with a list of colors") {
+                var colors: [NSColor]?
+
+                beforeEach {
+                    colors = [
+                        Colors.colorWithContrastRatio3,
+                        Colors.colorWithContrastRatio4_5,
+                        Colors.colorWithContrastRatio7
+                    ]
+                }
+
+                context("by not providing any passing color") {
+                    it("returns nil") {
+                        let actual = NSColor.getTextColor(fromColors: [], withFont: Fonts.smallFont, onBackgroundColor: Colors.white, conformanceLevel: .AA)
+
+                        expect(actual).to(beNil())
+                    }
+                }
+
+                context("when defining conformance level .AA") {
+                    context("when using a small font") {
+                        it("returns black") {
+                            let actual = NSColor.getTextColor(fromColors: colors!, withFont: Fonts.smallFont, onBackgroundColor: Colors.white, conformanceLevel: .AA)
+
+                            expect(actual).to(equal(Colors.colorWithContrastRatio4_5))
+                        }
+                    }
+
+                    context("when using a small bold font") {
+                        it("returns black") {
+                            let actual = NSColor.getTextColor(fromColors: colors!, withFont: Fonts.smallBoldFont, onBackgroundColor: Colors.white, conformanceLevel: .AA)
+
+                            expect(actual).to(equal(Colors.colorWithContrastRatio3))
+                        }
+                    }
+
+                    context("when using a large font") {
+                        it("returns black") {
+                            let actual = NSColor.getTextColor(fromColors: colors!, withFont: Fonts.largeFont, onBackgroundColor: Colors.white, conformanceLevel: .AA)
+
+                            expect(actual).to(equal(Colors.colorWithContrastRatio3))
+                        }
+                    }
+                }
+
+                context("when defining conformance level .AAA") {
+                    context("when using a small font") {
+                        it("returns black") {
+                            let actual = NSColor.getTextColor(fromColors: colors!, withFont: Fonts.smallFont, onBackgroundColor: Colors.white, conformanceLevel: .AAA)
+
+                            expect(actual).to(equal(Colors.colorWithContrastRatio7))
+                        }
+                    }
+
+                    context("when using a small bold font") {
+                        it("returns black") {
+                            let actual = NSColor.getTextColor(fromColors: colors!, withFont: Fonts.smallBoldFont, onBackgroundColor: Colors.white, conformanceLevel: .AAA)
+
+                            expect(actual).to(equal(Colors.colorWithContrastRatio4_5))
+                        }
+                    }
+
+                    context("when using a large font") {
+                        it("returns black") {
+                            let actual = NSColor.getTextColor(fromColors: colors!, withFont: Fonts.largeFont, onBackgroundColor: Colors.white, conformanceLevel: .AAA)
+
+                            expect(actual).to(equal(Colors.colorWithContrastRatio4_5))
+                        }
+                    }
+                }
+            }
+
+            context("when calling getBackgroundColor") {
+                context("by passing in a dark text color") {
+                    it("returns white") {
+                        let actual = NSColor.getBackgroundColor(forTextColor: Colors.black)
+
+                        expect(actual).to(equal(NSColor.white))
+                    }
+                }
+
+                context("by passing in a light text color") {
+                    it("returns black") {
+                        let actual = NSColor.getBackgroundColor(forTextColor: Colors.white)
+
+                        expect(actual).to(equal(NSColor.black))
+                    }
+                }
+
+                context("by passing in a text color with a medium relative luminance") {
+                    it("returns black") {
+                        let actual = NSColor.getBackgroundColor(forTextColor: Colors.colorWithContrastRatio4_5)
+
+                        expect(actual).to(equal(NSColor.black))
+                    }
+                }
+            }
+
+            context("when calling getBackgroundColor with a list of colors") {
+                var colors: [NSColor]?
+
+                beforeEach {
+                    colors = [
+                        Colors.colorWithContrastRatio3,
+                        Colors.colorWithContrastRatio4_5,
+                        Colors.colorWithContrastRatio7
+                    ]
+                }
+
+                context("by not providing any passing color") {
+                    it("returns nil") {
+                        let actual = NSColor.getBackgroundColor(fromColors: [], withFont: Fonts.smallFont, belowTextColor: Colors.white, conformanceLevel: .AA)
+
+                        expect(actual).to(beNil())
+                    }
+                }
+
+                context("when defining conformance level .AA") {
+                    context("when using a small font") {
+                        it("returns black") {
+                            let actual = NSColor.getBackgroundColor(fromColors: colors!, withFont: Fonts.smallFont, belowTextColor: Colors.white, conformanceLevel: .AA)
+
+                            expect(actual).to(equal(Colors.colorWithContrastRatio4_5))
+                        }
+                    }
+
+                    context("when using a small bold font") {
+                        it("returns black") {
+                            let actual = NSColor.getBackgroundColor(fromColors: colors!, withFont: Fonts.smallBoldFont, belowTextColor: Colors.white, conformanceLevel: .AA)
+
+                            expect(actual).to(equal(Colors.colorWithContrastRatio3))
+                        }
+                    }
+
+                    context("when using a large font") {
+                        it("returns black") {
+                            let actual = NSColor.getBackgroundColor(fromColors: colors!, withFont: Fonts.largeFont, belowTextColor: Colors.white, conformanceLevel: .AA)
+
+                            expect(actual).to(equal(Colors.colorWithContrastRatio3))
+                        }
+                    }
+                }
+
+                context("when defining conformance level .AAA") {
+                    context("when using a small font") {
+                        it("returns black") {
+                            let actual = NSColor.getBackgroundColor(fromColors: colors!, withFont: Fonts.smallFont, belowTextColor: Colors.white, conformanceLevel: .AAA)
+
+                            expect(actual).to(equal(Colors.colorWithContrastRatio7))
+                        }
+                    }
+
+                    context("when using a small bold font") {
+                        it("returns black") {
+                            let actual = NSColor.getBackgroundColor(fromColors: colors!, withFont: Fonts.smallBoldFont, belowTextColor: Colors.white, conformanceLevel: .AAA)
+
+                            expect(actual).to(equal(Colors.colorWithContrastRatio4_5))
+                        }
+                    }
+
+                    context("when using a large font") {
+                        it("returns black") {
+                            let actual = NSColor.getBackgroundColor(fromColors: colors!, withFont: Fonts.largeFont, belowTextColor: Colors.white, conformanceLevel: .AAA)
+
+                            expect(actual).to(equal(Colors.colorWithContrastRatio4_5))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#endif
