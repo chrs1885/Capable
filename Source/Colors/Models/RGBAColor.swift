df379c77944899ff8bceb241b02e6e4a9b64b18c
Christoph Wendt <christoph.wendt@me.com>
2019-01-16 22:19:29 +0100

    feature: extend RGBAColor with functionality to calculate WCAG compliant colors

--- Source/Colors/Models/RGBAColor.swift
+++ Source/Colors/Models/RGBAColor.swift
@@ -8,34 +8,14 @@
 import CoreGraphics
 
 struct RGBAColor: Equatable {
+    struct Colors {
+        static let white = RGBAColor(red: 255.0, green: 255.0, blue: 255.0, alpha: 1.0)
+        static let black = RGBAColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+    }
+
     var red, green, blue, alpha: CGFloat
 
-    static func getContrastRatio(forColor aColor: RGBAColor, andColor otherColor: RGBAColor) -> CGFloat {
-        var aColor = aColor
-        if aColor.alpha < 1.0 {
-            aColor = aColor.blended(withFraction: aColor.alpha, of: otherColor)
-        }
-
-        let luminance1 = aColor.relativeLuminance
-        let luminance2 = otherColor.relativeLuminance
-
-        return (max(luminance1, luminance2) + 0.05) / (min(luminance1, luminance2) + 0.05)
-    }
-
-    func blended(withFraction fraction: CGFloat,
-                 of color: RGBAColor) -> RGBAColor {
-        func getBlendedColorComponent(aColorComponent: CGFloat, fraction: CGFloat, otherColorComponent: CGFloat) -> CGFloat {
-            let blendedComponent = fraction * aColorComponent + (1 - fraction) * otherColorComponent
-            return blendedComponent
-        }
-
-        let red = getBlendedColorComponent(aColorComponent: self.red, fraction: self.alpha, otherColorComponent: color.red)
-        let green = getBlendedColorComponent(aColorComponent: self.green, fraction: self.alpha, otherColorComponent: color.green)
-        let blue = getBlendedColorComponent(aColorComponent: self.blue, fraction: self.alpha, otherColorComponent: color.blue)
-
-        return RGBAColor(red: red, green: green, blue: blue, alpha: 1.0)
-    }
-
+    // https://www.w3.org/TR/WCAG20-TECHS/G17.html
     var relativeLuminance: CGFloat {
         func getAdjustedColorComponent(_ colorComponent: CGFloat) -> CGFloat {
             if colorComponent <= 0.03928 {
@@ -50,5 +30,64 @@ struct RGBAColor: Equatable {
         let adjustedBlue = getAdjustedColorComponent(self.blue)
 
         return (0.2126 * adjustedRed) + (0.7152 * adjustedGreen) + (0.0722 * adjustedBlue)
+    }
+}
+
+// MARK: - Calculating color properties
+extension RGBAColor {
+    static func getContrastRatio(forTextColor textColor: RGBAColor, onBackgroundColor backgroundColor: RGBAColor) -> CGFloat {
+        let blendedColor = textColor.alpha < 1.0 ? textColor.blended(withFraction: textColor.alpha, ofColor: backgroundColor) : textColor
+
+        let textColorLuminance = blendedColor.relativeLuminance
+        let backgroundColorLuminance = backgroundColor.relativeLuminance
+
+        return (max(textColorLuminance, backgroundColorLuminance) + 0.05) / (min(textColorLuminance, backgroundColorLuminance) + 0.05)
+    }
+
+    static func isValidColorCombination(textColor: RGBAColor, fontProps: FontProps, onBackgroundColor backgroundColor: RGBAColor, conformanceLevel: ConformanceLevel) -> Bool {
+        let contrastRatio = RGBAColor.getContrastRatio(forTextColor: textColor, onBackgroundColor: backgroundColor)
+        let currentConformanceLevel = ConformanceLevel(contrastRatio: contrastRatio, fontProps: fontProps)
+
+        return currentConformanceLevel >= conformanceLevel
+    }
+}
+
+// MARK: - Text colors
+extension RGBAColor {
+    static func getTextColor(onBackgroundColor backgroundColor: RGBAColor) -> RGBAColor {
+        let luminance = backgroundColor.relativeLuminance
+
+        return luminance > 0.179 ? Colors.black : Colors.white
+    }
+}
+
+// MARK: - Background colors
+extension RGBAColor {
+    static func getBackgroundColor(forTextColor textColor: RGBAColor) -> RGBAColor {
+        if textColor.alpha < 1.0 {
+            let whiteContrastRatio = self.getContrastRatio(forTextColor: textColor, onBackgroundColor: Colors.white)
+            let blackContrastRatio = self.getContrastRatio(forTextColor: textColor, onBackgroundColor: Colors.black)
+
+            return whiteContrastRatio > blackContrastRatio ? Colors.white : Colors.black
+        } else {
+            return textColor.relativeLuminance > 0.179 ? Colors.black : Colors.white
+        }
+    }
+}
+
+// MARK: - Color operations
+extension RGBAColor {
+    func blended(withFraction fraction: CGFloat,
+                 ofColor color: RGBAColor) -> RGBAColor {
+        func getBlendedColorComponent(aColorComponent: CGFloat, fraction: CGFloat, otherColorComponent: CGFloat) -> CGFloat {
+            let blendedComponent = fraction * aColorComponent + (1 - fraction) * otherColorComponent
+            return blendedComponent
+        }
+
+        let red = getBlendedColorComponent(aColorComponent: self.red, fraction: self.alpha, otherColorComponent: color.red)
+        let green = getBlendedColorComponent(aColorComponent: self.green, fraction: self.alpha, otherColorComponent: color.green)
+        let blue = getBlendedColorComponent(aColorComponent: self.blue, fraction: self.alpha, otherColorComponent: color.blue)
+
+        return RGBAColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
 }
--- Tests/Colors/RGBAColorTests.swift
+++ Tests/Colors/RGBAColorTests.swift
@@ -11,55 +11,19 @@ import Nimble
 
 class RGBAColorTests: QuickSpec {
     override func spec() {
-        struct Color {
+        struct Colors {
             static let white = RGBAColor(red: 255.0, green: 255.0, blue: 255.0, alpha: 1.0)
-            static let black = RGBAColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
-            static let green = RGBAColor(red: 22.0, green: 100.0, blue: 59.0, alpha: 1.0)
-            static let orange = RGBAColor(red: 200.0, green: 110.0, blue: 59.0, alpha: 1.0)
             static let semiTransparentColor = RGBAColor(red: 0.5 * 255.0, green: 0.0, blue: 255.0, alpha: 0.75)
             static let blendedColor = RGBAColor(red: 0.625 * 255.0, green: 0.25 * 255.0, blue: 255.0, alpha: 1.0)
         }
 
         describe("The RGBAColor class") {
-            context("when calling getContrastRatio") {
-                context("by passing in the same color twice") {
-                    it("returns the minimum contrast ratio of 1.0") {
-                        expect(RGBAColor.getContrastRatio(forColor: Color.white, andColor: Color.white)).to(equal(1.0))
-                    }
-                }
-
-                context("by passing in white and black") {
-                    it("returns the maximum contrast ratio of 21.0") {
-                        expect(RGBAColor.getContrastRatio(forColor: Color.white, andColor: Color.black)).to(equal(21.0))
-                    }
-                }
-
-                context("by passing in green and orange color") {
-                    it("returns a contrast ratio of 1.96") {
-                        let actualContrastRatio = RGBAColor.getContrastRatio(forColor: Color.orange, andColor: Color.green)
-                        let rounded = floor(actualContrastRatio * 100) / 100
-
-                        expect(rounded).to(equal(1.96))
-                    }
-                }
-
-                context("by passing in semi transparent color and white") {
-                    it("returns a contrast ratio of 4.51") {
-                        let actualContrastRatio = RGBAColor.getContrastRatio(forColor: Color.semiTransparentColor, andColor: Color.white)
-                        let rounded = floor(actualContrastRatio * 100) / 100
-
-                        expect(rounded).to(equal(4.51))
-                    }
-                }
-            }
-
             context("when calling getBlendedColor") {
                 context("by passing in a semi transparent color and a opaque color") {
-                    it("returns the minimum contrast ratio of 1.0") {
-                        let actual = Color.semiTransparentColor.blended(withFraction: Color.semiTransparentColor.alpha, of: Color.white)
-                        let expected = Color.blendedColor
+                    it("returns a color blended with its background color") {
+                        let actual = Colors.semiTransparentColor.blended(withFraction: Colors.semiTransparentColor.alpha, ofColor: Colors.white)
 
-                        expect(actual).to(equal(expected))
+                        expect(actual).to(equal(Colors.blendedColor))
                     }
                 }
             }
