353b2eba5c5d5fff08948b71e9542e1e24f2a02a
Christoph Wendt <christoph.wendt@me.com>
2019-01-24 21:00:15 +0100

    chore: add demo screen to ios example

---
+++ Example/Source/iOS/ColorsViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ColorsViewController.swift
+//  Example-iOS
+//
+//  Created by Christoph Wendt on 21.01.19.
+//  Copyright © 2019 Christoph Wendt. All rights reserved.
+//
+
+import UIKit
+import Capable
+
+class ColorsViewController: UIViewController {
+    @IBOutlet weak var textColorButton: UIButton!
+    @IBOutlet weak var backgroundColorButton: UIButton!
+    @IBOutlet weak var contrastRatioLabel: UILabel!
+    @IBOutlet weak var aaSmallTextView: ConformanceLevelView!
+    @IBOutlet weak var aaaSmallTextView: ConformanceLevelView!
+    @IBOutlet weak var aaLargeTextView: ConformanceLevelView!
+    @IBOutlet weak var aaaLargeTextView: ConformanceLevelView!
+
+    var textColor: UIColor = .lightGray
+    var backgroundColor: UIColor = .black
+    var contrastRatio: CGFloat?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.contrastRatio = UIColor.getContrastRatio(forTextColor: self.textColor, onBackgroundColor: self.backgroundColor)!
+        setupView()
+    }
+}
+
+// MARK: - Updating Views
+extension ColorsViewController {
+    func setupView() {
+        updateColorButton(button: self.textColorButton, withColor: self.textColor)
+        updateColorButton(button: self.backgroundColorButton, withColor: self.backgroundColor)
+        updateContrastRatioLabel()
+
+        self.aaSmallTextView.text = "AA"
+        self.aaaSmallTextView.text = "AAA"
+        self.aaLargeTextView.text = "AA (Large Text)"
+        self.aaaLargeTextView.text = "AAA (Large Text)"
+        updateConformanceLevelViews()
+    }
+
+    func updateColorButton(button: UIButton, withColor color: UIColor) {
+        button.backgroundColor = color
+        let textColor = UIColor.getTextColor(onBackgroundColor: color)!
+        button.setTitleColor(textColor, for: .normal)
+    }
+
+    func updateConformanceLevelViews() {
+        guard let contrastRatio = self.contrastRatio else { return }
+
+        self.aaSmallTextView.isPassing = contrastRatio >= 4.5
+        self.aaaSmallTextView.isPassing = contrastRatio >= 7.0
+        self.aaLargeTextView.isPassing = contrastRatio >= 3.0
+        self.aaaLargeTextView.isPassing = contrastRatio >= 4.5
+    }
+
+    func updateContrastRatioLabel() {
+        guard let contrastRatio = self.contrastRatio else { return }
+        self.contrastRatioLabel.text = String(format: "%.2f", contrastRatio)
+    }
+}
+
+// MARK: - Handling User Interaction
+extension ColorsViewController {
+    @IBAction func colorButtonPressed(_ sender: Any) {
+        let button = sender as! UIButton
+        let colorType: ColorType = button == self.textColorButton ? .textColor : .backgroundColor
+        let color = colorType == .textColor ? self.textColor : self.backgroundColor
+        let selectionHandler: (UIColor) -> () = {
+            selectedColor in
+
+            if colorType == .textColor {
+                self.textColor = selectedColor
+            } else {
+                self.backgroundColor = selectedColor
+            }
+
+            self.contrastRatio = UIColor.getContrastRatio(forTextColor: self.textColor, onBackgroundColor: self.backgroundColor)!
+            self.updateColorButton(button: button, withColor: selectedColor)
+            self.updateContrastRatioLabel()
+            self.updateConformanceLevelViews()
+        }
+
+        let colorAlert = ColorAlertController.create(withColorType: colorType, color: color, prefferedStyle: .actionSheet, selectionHandler: selectionHandler)
+        self.present(colorAlert, animated: true, completion: nil)
+    }
+}
---
+++ Example/Source/iOS/ConformanceLevelView.swift
@@ -0,0 +1,50 @@
+//
+//  ConformanceLevelView.swift
+//  Example-iOS
+//
+//  Created by Christoph Wendt on 21.01.19.
+//  Copyright © 2019 Christoph Wendt. All rights reserved.
+//
+
+import UIKit
+
+class ConformanceLevelView: UIView {
+    struct Colors {
+        static let green = UIColor(red: 0.0, green: 135 / 255.0, blue: 0.0, alpha: 1.0)
+        static let red = UIColor(red: 220 / 255.0, green: 0.0, blue: 0.0, alpha: 1.0)
+    }
+
+    private let textLabel = UILabel()
+
+    var text: String = "" {
+        didSet {
+            self.textLabel.text = text
+        }
+    }
+
+    var isPassing: Bool = false {
+        didSet {
+            self.backgroundColor = isPassing ? Colors.green : Colors.red
+            self.textLabel.textColor = UIColor.getTextColor(onBackgroundColor: self.backgroundColor!)!
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+
+    func setupView() {
+        self.addSubview(textLabel)
+        self.textLabel.font = UIFont.scaledSystemFont(ofSize: 13.0)
+
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        let views: [String: Any] = ["textLabel": self.textLabel]
+        var allConstraints: [NSLayoutConstraint] = []
+        allConstraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[textLabel]-8-|", metrics: nil, views: views)
+        allConstraints += NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[textLabel]-8-|", metrics: nil, views: views)
+        NSLayoutConstraint.activate(allConstraints)
+
+        self.isPassing = true
+    }
+}
