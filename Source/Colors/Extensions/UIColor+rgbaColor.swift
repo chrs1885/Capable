97736b1a217557434258a0bd3af72fb2cd7fd2f7
Christoph Wendt <christoph.wendt@me.com>
2019-01-31 22:34:15 +0100

    chore: normalize values with component > 255.0 or < 0.0

--- Source/Colors/Extensions/UIColor+rgbaColor.swift
+++ Source/Colors/Extensions/UIColor+rgbaColor.swift
@@ -11,12 +11,20 @@ import UIKit
 
 extension UIColor {
     var rgbaColor: RGBAColor? {
+        func normalize(_ component: CGFloat) -> CGFloat {
+            let nomralizedValue = component * 255.0
+
+            if nomralizedValue > 255.0 { return 255.0 }
+            if nomralizedValue < 0.0 { return 0.0 }
+            return nomralizedValue
+        }
+
         var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, white: CGFloat = 0, alpha: CGFloat = 0
 
         if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
-            return RGBAColor(red: (red * 255.0), green: (green * 255.0), blue: (blue * 255.0), alpha: alpha)
+            return RGBAColor(red: normalize(red), green: normalize(green), blue: normalize(blue), alpha: alpha)
         } else if self.getWhite(&white, alpha: &alpha) {
-            let white = white * 255.0
+            let white = normalize(white)
             return RGBAColor(red: white, green: white, blue: white, alpha: alpha)
         } else {
             Logger.warning("Input color not compatible with sRGB color space.")
