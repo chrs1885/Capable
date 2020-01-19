9d4af5102921a7682c714ecc0ab02e16b44726a0
Christoph Wendt <christoph.wendt@dm.de>
2020-01-19 17:23:30 +0100

    chore: add pods dir to support carthage

---
+++ Example/Pods/Target Support Files/Capable-macOS/Capable-macOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Capable_macOS : NSObject
+@end
+@implementation PodsDummy_Capable_macOS
+@end
---
+++ Example/Pods/Target Support Files/Capable-watchOS/Capable-watchOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Capable_watchOS : NSObject
+@end
+@implementation PodsDummy_Capable_watchOS
+@end
